f58abe918ed2f21a8d9240d54bbb51fb304c2221
Daniel joseph ml <mldanieljoseph@gmail.com>
2024-03-17 23:04:55 +0530

    secound phase hasbeen complate

--- Todo/Todo_table/urls.py
+++ Todo/Todo_table/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('/<int:pk>/', views.index, name='index'),
     path('read/', views.read, name='read'),
 ]
 
--- Todo/Todo_table/views.py
+++ Todo/Todo_table/views.py
@@ -1,9 +1,26 @@
 from django.shortcuts import render
+from django.contrib import messages
+from .models import Todo
+
 
 
 # Create your views here.
 def index(request):
-    return  render(request, 'index.html')
+    obj=Todo.objects.all()
+    if request.POST and 'add_new' in request.POST:
+       title=request.POST['title']
+       description=request.POST['desc']
+       type=request.POST['type']
+       ins_obj=Todo.objects.create(title=title, description=description,type=type)
+       ins_obj.save()
+       messages.success(request, 'Todo added successfully')
+       obj=Todo.objects.all()
+       return  render(request, 'index.html', {'obj':obj})
+    else:
+        if request.POST:
+            messages.error(request, 'Todo not added')
+  
+    return  render(request, 'index.html',{'obj':obj})
 
 def read(request):
     return  render(request, 'result.html')
