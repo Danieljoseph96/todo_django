9f792639fbacbc713716b5e13fd4f70abd1776f6
Daniel joseph ml <mldanieljoseph@gmail.com>
2024-03-17 07:53:54 +0530

    secound phase has been complate today prject wil complate

--- Todo/Todo_table/models.py
+++ Todo/Todo_table/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class Todo(models.Model):
+    crital=1
+    normal=0
+    type_level=[
+        (crital, 'crital'),
+        (normal, 'normal'),
+    ]
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    created_date = models.DateTimeField(auto_now_add=True)
+    type=models.IntegerField(choices=type_level, default=normal)  
+    updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
